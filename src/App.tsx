4d97b5b8101138533aee4b8978d6df4ca2380ffe
Nemanja Mitic <mitic.nemanja@hotmail.com>
2025-07-02 22:42:04 +0200

    fix: Show data in component bill table

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-
-import './App.css'
+import { Container } from '@mui/material';
+import './App.css';
+import { BillTable } from './components/BillTable';
 
 function App() {
-
   return (
-    <>
-      <h1>
-        Hello
-      </h1>
-    </>
-  )
+    <Container>
+      <h1>Bill Information</h1>
+      <BillTable />
+    </Container>
+  );
 }
 
-export default App
+export default App;
