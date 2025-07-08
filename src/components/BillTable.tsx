c1f50e6e376622da7b99bf0c45f14e7355df4f2a
Nemanja Mitic <mitic.nemanja@hotmail.com>
2025-07-08 12:52:08 +0200

    refactor: Drpdown list of filter status

--- src/components/BillTable.tsx
+++ src/components/BillTable.tsx
@@ -13,7 +13,7 @@ import {
 	TableContainer,
 	TableHead,
 	TableRow,
-	Typography,
+	Typography
 } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import { useMemo, useState } from "react";
@@ -43,7 +43,7 @@ export const BillTable = ({ onRowClick }: BillTableProps) => {
 		setSelectedBill(null);
 	};
 
-	const handleFilterChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+	const handleFilterChange = (event: React.ChangeEvent<{ value: string }>) => {
 		setFilterStatus(event.target.value as string);
 	};
 
@@ -69,126 +69,136 @@ export const BillTable = ({ onRowClick }: BillTableProps) => {
 		);
 
 	return (
-		<Container>
-			<Box>
-				<FormControl sx={{ my: 5, minWidth: 200 }}>
-					<InputLabel id="bill-status-select-label">
-						Filter by Status
-					</InputLabel>
-					<Select
-						labelId="bill-status-select-label"
-						id="bill-status-select"
-						value={filterStatus}
-						label="Filter by Status"
-						//@ts-ignore
-						onChange={handleFilterChange}
-						sx={{
-							color: "#000",
-							"& .MuiOutlinedInput-notchedOutline": {
-								borderColor: "#000",
-							},
-							"& .MuiSvgIcon-root": {
-								color: "#000",
-							},
-							"&:hover .MuiOutlinedInput-notchedOutline": {
-								borderColor: "#000",
-							},
-							"&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-								borderColor: "#000",
-							},
-						}}
-						MenuProps={{
-							PaperProps: {
-								sx: {
-									backgroundColor: "GrayText",
-									color: "#000",
-								},
-							},
-						}}
-					>
-						<MenuItem value="">
-							<em>All Statuses</em>
-						</MenuItem>
-						{uniqueBillStatuses.map((status) => (
-							<MenuItem key={status} value={status}>
-								{status}
-							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-			</Box>
-			<Box
-				sx={{
-					padding: 1,
-					backgroundColor: "GrayText",
-					color: "#1565c0",
-					margin: "2px",
-					marginTop: "10px",
-					width: "90%",
-					borderRadius: 2,
-				}}
-			>
-				<TableContainer component={Paper}>
-					<Table>
-						<TableHead>
-							<TableRow>
-								<TableCell>Icon</TableCell>
-								<TableCell>Bill Number</TableCell>
-								<TableCell>Bill Type</TableCell>
-								<TableCell>Bill Status</TableCell>
-								<TableCell>Sponsor</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody sx={{ cursor: "pointer" }}>
-							{filteredBills?.length === 0 ? (
-								<TableRow>
-									<TableCell colSpan={5} align="center">
-										<Typography variant="subtitle1" color="textSecondary">
-											No bills to display
-										</Typography>
-									</TableCell>
-								</TableRow>
-							) : (
-								filteredBills?.map((bill, index) => (
-									<TableRow
-										key={bill.bill_id || index}
-										onClick={() => handleRowClick(bill)}
-									>
-										<TableCell>
-											<IconButton aria-label="star">
-												<StarIcon color="disabled" />
-											</IconButton>
-										</TableCell>
-										<TableCell>{bill.billNo}</TableCell>
-										<TableCell>{bill.billType}</TableCell>
-										<TableCell>{bill.bill_status}</TableCell>
-										<TableCell>
-											{bill.sponsors.map(
-												(sponsor) => sponsor.sponsor.as.showAs,
-											)}
-										</TableCell>
-									</TableRow>
-								))
-							)}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</Box>
-			<Box
-				sx={{
-					display: "flex",
-					justifyContent: "center",
-					width: "100%",
-					marginTop: "50px",
-				}}
-			>
-				<Pagination page={2} color="standard" size="large" />
-			</Box>
-			<BillModal
-				open={isModalOpen}
-				onClose={handleCloseModal}
-				bill={selectedBill} // Pass the selected bill to the modal
-			/>
-		</Container>
-	);
+    <Container>
+      <Box>
+        <FormControl sx={{ my: 5, minWidth: 200 }}>
+          <InputLabel id="bill-status-select-label">Filter by Status</InputLabel>
+          <Select
+            labelId="bill-status-select-label"
+            id="bill-status-select"
+            value={filterStatus}
+            label="Filter by Status"
+            //@ts-ignore
+            onChange={handleFilterChange}
+            sx={{
+              color: '#000',
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: '#000',
+              },
+              '& .MuiSvgIcon-root': {
+                color: '#000',
+              },
+              '&:hover .MuiOutlinedInput-notchedOutline': {
+                borderColor: '#000',
+              },
+              '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: '#000',
+              },
+            }}
+            MenuProps={{
+              PaperProps: {
+                sx: {
+                  backgroundColor: '#fff',
+                  color: '#000',
+                  border: '1px solid #000',
+                },
+              },
+            }}
+          >
+            <MenuItem value="">
+              <em>All Statuses</em>
+            </MenuItem>
+            <MenuItem value="Current">
+              <p>Current</p>
+            </MenuItem>
+            <MenuItem value="Withdrawn">
+              <p>Withdrawn</p>
+            </MenuItem>
+            <MenuItem value="Enacted">
+              <p>Enacted</p>
+            </MenuItem>
+            <MenuItem value="Rejected">
+              <p>Rejected</p>
+            </MenuItem>
+            <MenuItem value="Defeated">
+              <p>Defeated</p>
+            </MenuItem>
+            <MenuItem value="Lapsed">
+              <p>Lapsed</p>
+            </MenuItem>
+            {uniqueBillStatuses.map((status) => (
+              <MenuItem key={status} value={status}>
+                {status}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box
+        sx={{
+          padding: 1,
+          backgroundColor: 'GrayText',
+          color: '#1565c0',
+          margin: '2px',
+          marginTop: '10px',
+          width: '90%',
+          borderRadius: 2,
+        }}
+      >
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Icon</TableCell>
+                <TableCell>Bill Number</TableCell>
+                <TableCell>Bill Type</TableCell>
+                <TableCell>Bill Status</TableCell>
+                <TableCell>Sponsor</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody sx={{ cursor: 'pointer' }}>
+              {filteredBills?.length === 0 ? (
+                <TableRow>
+                  <TableCell colSpan={5} align="center">
+                    <Typography variant="subtitle1" color="textSecondary">
+                      No bills to display
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              ) : (
+                filteredBills?.map((bill, index) => (
+                  <TableRow key={bill.bill_id || index} onClick={() => handleRowClick(bill)}>
+                    <TableCell>
+                      <IconButton aria-label="star">
+                        <StarIcon color="disabled" />
+                      </IconButton>
+                    </TableCell>
+                    <TableCell>{bill.billNo}</TableCell>
+                    <TableCell>{bill.billType}</TableCell>
+                    <TableCell>{bill.bill_status}</TableCell>
+                    <TableCell>{bill.sponsors.map((sponsor) => sponsor.sponsor.as.showAs)}</TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%',
+          marginTop: '50px',
+        }}
+      >
+        <Pagination page={2} color="standard" size="large" />
+      </Box>
+      <BillModal
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        bill={selectedBill} // Pass the selected bill to the modal
+      />
+    </Container>
+  );
 };
